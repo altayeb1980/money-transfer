2c235327a624ecafeb7bf40112c3534dc57ab38c
Altayeb Saadeh <loay.saada@gmail.com>
2019-01-16 16:20:30 +0100

    apply the flatJar

---
+++ target/classes/db/migration/V1__Create_monery_transfer_tables.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (UserId LONG PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ UserName VARCHAR(30) NOT NULL,
+ EmailAddress VARCHAR(30) NOT NULL);
+
+CREATE UNIQUE INDEX idx_ue on User(UserName,EmailAddress);
+
+DROP TABLE IF EXISTS Account;
+
+CREATE TABLE Account (AccountId LONG PRIMARY KEY AUTO_INCREMENT NOT NULL,
+UserName VARCHAR(30),
+Balance DECIMAL(19,4),
+CurrencyCode VARCHAR(30)
+);
+
+CREATE UNIQUE INDEX idx_acc on Account(UserName,CurrencyCode);
---
+++ target/classes/db/migration/V2__Add_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO User (UserName, EmailAddress) VALUES ('altayeb','altayeb@gmail.com');
+INSERT INTO User (UserName, EmailAddress) VALUES ('pawel','pawel@gmail.com');
+INSERT INTO User (UserName, EmailAddress) VALUES ('asia','asia@gmail.com');
+
+
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('altayeb',100.0000,'USD');
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('pawel',200.0000,'USD');
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('altayeb',500.0000,'EUR');
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('pawel',500.0000,'EUR');
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('altayeb',500.0000,'GBP');
+INSERT INTO Account (UserName,Balance,CurrencyCode) VALUES ('pawel',500.0000,'GBP');
